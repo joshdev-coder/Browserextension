1d90594b85b7070536163a95927f142b4a4f575c
Challmejosh <challmejosh@gmail.com>
2025-04-04 04:56:14 -0700

    first commit

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import { AppContext } from "./components/Context";
 export default function Home() {
   const {bg} = useContext(AppContext)
   return (
-    <div className={` ${bg === 'sun' && 'bg-[#d6e2f5]'  } bg-[#09153e] w-full flex gap-8 flex-col text-white items-center justify-center`}>
+    <div className={` ${bg === 'sun' && 'bg-[#d6e2f5]' } bg-[#09153e] w-full flex gap-8 flex-col text-white items-center justify-center`}>
       <div className="mx-5 sm:mx-10  md:mx-32 flex flex-col gap-8 py-3 ">
         <SearchBar />
         <Switch />
