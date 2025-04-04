1d9d28743a933d56115a7a412dc041c7aa2eb939
Challmejosh <challmejosh@gmail.com>
2025-04-04 03:40:59 -0700

    first commit

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   const {bg} = useContext(AppContext)
   return (
     <div className={` ${bg === 'sun' && 'bg-[#d6e2f5]'  } bg-[#09153e] w-full flex gap-8 flex-col text-white items-center justify-center`}>
-      <div className="mx-5 sm:mx-10 h-screen overflow-y-scroll [&::-webkit-scrollbar]:hidden scrollbar md:mx-32 flex flex-col gap-8 py-3 ">
+      <div className="mx-5 sm:mx-10  md:mx-32 flex flex-col gap-8 py-3 ">
         <SearchBar />
         <Switch />
       </div>
